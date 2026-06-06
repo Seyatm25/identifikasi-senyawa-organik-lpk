615b974ad98759744611443c23e8881de6422020
Seyatm25 <ghazanaqh@gmail.com>
2026-06-06 09:33:02 +0700

    Update idenfikasi senyawa organik.py

--- idenfikasi senyawa organik.py
+++ idenfikasi senyawa organik.py
@@ -639,7 +639,7 @@ elif pilihan_halaman == "📕 BAB IV. ASAM KARBOKSILAT DAN DERIVATNYA":
     st.latex(r"3\text{R-CONH-OH} + \text{FeCl}_3 \rightarrow \text{Fe(R-CONHO)}_3 \text{ (Kompleks Ungu)} + 3\text{HCl}")
 
 elif pilihan_halaman == "🔬 POST TEST":
-    st.title("🔀 Smart Flowchart Auto-Analyzer (Step-by-Step)")
+    st.title("🔀 Asisten Identifikasi Cerdas (Step-by-Step)")
     st.write("Sistem ini mensimulasikan penelusuran Identifikasi Kualitatif Golongan Fungsi secara otomatis berderet.")
 
     if not st.session_state.test_started:
